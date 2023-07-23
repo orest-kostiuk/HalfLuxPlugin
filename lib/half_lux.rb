e635e4b94eac043bab45c845ede8ef35b460adb1
Orest Kostiuk <okostiuk@assurance.com>
2023-07-23 13:51:16 +0200

    fixed bug

---
+++ lib/half_lux.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative "HalfLuxPlugin/version"
+
+module HalfLux
+  PLACE_COUNT = 2
+
+  def places
+    PLACE_COUNT
+  end
+
+  def price
+    500
+  end
+
+  def available
+    self.tenants.count < PLACE_COUNT
+  end
+end
